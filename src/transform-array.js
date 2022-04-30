d32e3145509d7833d760743330052b28931849ce
Roman <rekun.olya@gmail.com>
2022-04-30 23:23:47 +0300

    feat: fix some tests on transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,44 +13,70 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(arr) {
- // throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-let rez = []
-if(!Array.isArray(arr)) {
-  throw new Error("'arr' parameter must be an instance of the Array!")
-}
-
-if(arr.length == 0) {
-  return rez
-}
-let array = arr
-array.map(el => {
-  console.log(el)
-  if(el !== '--discard-prev' || el !== '--double-prev' || el !== '--double-next' || el !== '--discard-next'){
-    rez.push(el)
+ function transform(arr) {
+  // throw new NotImplementedError('Not implemented');
+   // remove line with error and write your code here
+  let rez = []
+  if(!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!")
   }
-})
+  
+  if(arr.length === 0) {
+    return rez
+  }
+  let array = arr
 
-if(array.length === rez.length) {
-  return rez
-}
-    for (let i = 0; i < array.length; i++) {
-      if (array[0] == '--discard-prev' || array[0] == '--double-prev') {
-        rez.push(array.splice(0,1))
-        return rez
-      }
-      if (array[array.length-1] == '--double-next' || array[array.length-1] == '--discard-next') {
-        rez.push(arr.splice(-1,1))
-        return rez
-      }
-      // if (arr[i] == '--discard-next'){
-      //   rez.push(arr.splice(arr[i], 1 ))
-      //   return rez
-      // }
+  array.map(el => {
+    if(el !== '--discard-prev' && el !== '--double-prev' && 
+       el !== '--double-next' && el !== '--discard-next'){
+      rez.push(el)
     }
+  })
+  
+  if(array.length === rez.length) {
+    return rez
+  }
+      
+  if (arr[0] === '--discard-prev' || arr[0] === '--double-prev') {
+    array = arr.slice(1)
+    rez = array
+    console.log('rez', rez)
+    return rez
+  }
+  if (array[array.length-1] === '--double-next' 
+      || array[array.length-1] === '--discard-next') {
+        console.log('i m here')
+    array = arr.slice(0, arr.length - 1)
+    rez = array
+    return rez
+  }
+  for (let i = 0; i < arr.length; i++){
+    if(arr[i] === '--discard-prev') {
+      array = arr.slice(0, i-1).concat(arr.slice(i+1))
+      console.log('array', array)
+      rez = array
+    }
+    if(arr[i] === '--discard-next') {
+      array = arr.slice(0, i).concat(arr.slice(i+2))
+      console.log('arrayNext', array)
+      rez = array
+    }
+    if(arr[i] === '--double-prev') {
+      array = arr.slice(0, i).concat(arr[i-1]).concat(arr.slice(i+1))
+      console.log('arrayDoublePrev', array)
+      rez = array
+    }
+    if(arr[i] === '--double-next') {
+      array = arr.slice(0, i).concat(arr[i+1]).concat(arr.slice(i+1))
+      console.log('arrayDoubleNext', array)
+      rez = array
+    }
+  }
+  console.log(array)
+  console.log('rez', rez)
+  return rez
 
-}
+ }
 
 module.exports = {
   transform
