442b7d052931cd678c26611a914481f8f05a9902
rekunolya <rekun.olya@gmail.com>
2025-02-06 15:04:27 +0300

    feat: add a solution to task what-season

--- src/what-season.js
+++ src/what-season.js
@@ -12,29 +12,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function getSeason(date) {
-  //throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
   if( arguments.length == 0) {
     return 'Unable to determine the time of year!'
   }
-  if (!date.getTime) {
+  if (!Date.parse(date)) {
+    throw new Error('Invalid date!');
+  }
+  try {
+    date.valueOf();
+  } catch (error) {
     throw new Error('Invalid date!');
   }
 
-
-    if (date.getMonth() >= 0  && date.getMonth() <= 1 || date.getMonth() == 11 ){
-      return 'winter'
-    }
-    else if (date.getMonth() >= 2 && date.getMonth() <= 4){
-      return 'spring'
-    }
-    else if (date.getMonth() >= 5 && date.getMonth() <= 7){
-      return 'summer'
-    }
-    else if (date.getMonth() >= 8 && date.getMonth() <= 10){
-      return 'autumn'
-    }
-    
+  if (date.getMonth() >= 0  && date.getMonth() <= 1 || date.getMonth() == 11 ){
+    return 'winter';
+  } else if (date.getMonth() >= 2 && date.getMonth() <= 4){
+    return 'spring';
+  } else if (date.getMonth() >= 5 && date.getMonth() <= 7){
+    return 'summer';
+  } else if (date.getMonth() >= 8 && date.getMonth() <= 10){
+    return 'autumn';
+  }   
 }
 
 module.exports = {
